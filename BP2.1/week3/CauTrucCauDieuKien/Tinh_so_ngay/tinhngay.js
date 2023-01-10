5d158fa6fbcc879470326e8e5b371ddd3c9826c5
anhkhonglanman <quyen283hn@gmail.com>
2023-01-10 09:11:18 +0700

    bai tap

---
+++ BP2.1/week3/CauTrucCauDieuKien/Tinh_so_ngay/tinhngay.js
@@ -0,0 +1,17 @@
+
+let thang_1 = "1";
+let thang_2 = "2";
+let thang_3 = "3";
+let thang_4 = "4";
+let thang_5 = "5";
+let thang_6 = "6";
+let thang_7 = "7";
+let thang_8 = "8";
+let thang_9 = "9";
+let thang_10 = "10";
+let thang_11 = "11";
+let thang_12 = "12";
+
+function check_month() {
+    
+}
