3ed762be0e80c4a62334ca94c15956b61cd08895
anhkhonglanman <quyen283hn@gmail.com>
2023-01-11 09:36:20 +0700

    bai tap

---
+++ BP2.1/week3/CauTrucCauDieuKien/Ung_dung_may_tinh/main.js
@@ -0,0 +1,15 @@
+
+
+let view = document.getElementById('view');
+function inputValue(value) {
+    view.value += value
+}
+
+function result() {
+    let result = eval(view.value);
+    view.value = result;
+}
+
+function xoa() {
+    view.value = '';
+}
---
+++ BP2.1/week3/Thuc_hanh_loop/for/main.js
@@ -0,0 +1,6 @@
+let num = prompt('Enter number')
+let total = 0;
+for (var i = 0; i <= num; i +=1) {
+    total += i;
+    alert (total)
+}
